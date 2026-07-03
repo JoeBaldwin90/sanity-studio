38f5235aa364aa2523447dbbad9d911b1567aa1a
Joe Baldwin <joebaldwin@Joes-MacBook-Pro.local>
2026-07-03 23:37:10 +0100

    Document type icons

--- schemaTypes/venueType.ts
+++ schemaTypes/venueType.ts
@@ -1,9 +1,11 @@
 import { defineField, defineType } from 'sanity'
+import { PinIcon } from '@sanity/icons'
 
 export const venueType = defineType({
   type: "document",
   name: "venue",
   title: "Venue",
+  icon: PinIcon,
   fields: [
     defineField({
       type: "string",
